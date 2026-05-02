ea11116c33126ae25f9870a059f02103a2978a46
ritarock <dvryz1204@gmail.com>
2026-05-03 03:39:02 +0900

    refactor

---
+++ src/app/count.rs
@@ -0,0 +1,62 @@
+use anyhow::Result;
+use super::Reader;
+
+pub struct CountExecutor<R: Reader> {
+    reader: R,
+}
+
+impl<R: Reader> CountExecutor<R> {
+    pub fn new(reader: R) -> Self {
+        Self { reader }
+    }
+
+    pub fn execute(&self, filepath: &str) -> Result<usize> {
+        let rows = self.reader.read(filepath, true)?;
+        Ok(rows.len())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+
+    struct MockReader {
+        data: Vec<Vec<String>>,
+    }
+
+    impl Reader for MockReader {
+        fn read(&self, _filepath: &str, _disable_header: bool) -> Result<Vec<Vec<String>>> {
+            Ok(self.data.clone())
+        }
+    }
+
+    #[test]
+    fn test_execute_count() -> Result<()> {
+        let mock_data = vec![
+            vec!["id".to_string(), "name".to_string()],
+            vec!["1".to_string(), "name1".to_string()],
+        ];
+        let reader = MockReader { data: mock_data };
+        let executor = CountExecutor::new(reader);
+
+        let result = executor.execute("")?;
+
+        assert_eq!(result, 2);
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_execute_count_empty() -> Result<()> {
+        let mock_data = vec![];
+        let reader = MockReader { data: mock_data };
+        let executor = CountExecutor::new(reader);
+
+        let result = executor.execute("")?;
+
+        assert_eq!(result, 0);
+
+        Ok(())
+    }
+}
---
+++ src/app/header.rs
@@ -0,0 +1,70 @@
+use anyhow::{Result, anyhow};
+use super::Reader;
+
+pub struct HeaderExecutor<R: Reader> {
+    reader: R,
+}
+
+impl<R: Reader> HeaderExecutor<R> {
+    pub fn new(reader: R) -> Self {
+        Self { reader }
+    }
+
+    pub fn execute(&self, filepath: &str) -> Result<String> {
+        let rows = self.reader.read(filepath, false)?;
+
+        let first = rows.get(0).ok_or_else(|| anyhow!("no rows"))?;
+
+        let header = first
+            .iter()
+            .map(|s| s.trim())
+            .collect::<Vec<_>>()
+            .join(", ");
+        Ok(header)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use anyhow::Result;
+
+    struct MockReader {
+        data: Vec<Vec<String>>,
+    }
+
+    impl Reader for MockReader {
+        fn read(&self, _filepath: &str, _disable_header: bool) -> Result<Vec<Vec<String>>> {
+            Ok(self.data.clone())
+        }
+    }
+
+    #[test]
+    fn test_execute_header() -> Result<()> {
+        let mock_data = vec![
+            vec!["id".to_string(), "name".to_string()],
+            vec!["1".to_string(), "name1".to_string()],
+        ];
+        let reader = MockReader { data: mock_data };
+        let executor = HeaderExecutor::new(reader);
+
+        let result = executor.execute("")?;
+
+        assert_eq!(result, "id, name");
+
+        Ok(())
+    }
+
+    #[test]
+    fn test_execute_header_empty() -> Result<()> {
+        let mock_data = vec![];
+        let reader = MockReader { data: mock_data };
+        let executor = HeaderExecutor::new(reader);
+
+        let result = executor.execute("");
+
+        assert!(result.is_err());
+
+        Ok(())
+    }
+}
